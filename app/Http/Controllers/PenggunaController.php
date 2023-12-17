34e07be40f8bad8d80a851dc57e43cc00b89bb63
ujiherliana <ujiherliana@gmail.com>
2023-12-17 12:01:35 +0700

    first commit

---
+++ app/Http/Controllers/PenggunaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pengguna;
+use Illuminate\Http\Request;
+
+class PenggunaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $pengguna = Pengguna::orderBy('id', 'desc')->get();
+        return view('pengguna.index', compact('pengguna'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pengguna.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pengguna $pengguna)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pengguna $pengguna)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pengguna $pengguna)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pengguna $pengguna)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_12_12_014159_create_penggunas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penggunas', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_hp')->unique();
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->string('thumnail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penggunas');
+    }
+};
